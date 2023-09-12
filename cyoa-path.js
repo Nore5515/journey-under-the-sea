f6c2605cecad3584d718cd7b9daeb8fcd4489594
Noah Reed <Nore5515@gmail.com>
2023-09-12 15:39:04 -0500

    tedious

---
+++ cyoa-path.js
@@ -0,0 +1,230 @@
+const cyoa_paths = [
+    {
+        page: 3,
+        choices: ["6", "5"]
+    },
+    {
+        page: 5,
+        choices: ["8", "9"]
+    },
+    {
+        page: 6,
+        choices: ["10", "12"]
+    },
+    {
+        page: 8,
+        choices: ["11", "15"]
+    },
+    {
+        page: 9,
+        choices: ["16", "14"]
+    },
+    {
+        page: 10,
+        choices: ["17", "18"]
+    },
+    {
+        page: 11,
+        choices: ["24", "22"]
+    },
+    {
+        page: 12,
+        choices: ["21", "19"]
+    },
+    {
+        page: 14,
+        choices: ["26", "28"]
+    },
+    {
+        page: 15,
+        choices: ["23", "27"]
+    },
+    {
+        page: 16,
+        choices: ["29", "31"]
+    },
+    {
+        page: 17,
+        choices: ["32", "33"]
+    },
+    {
+        page: 18,
+        choices: ["34", "37"]
+    },
+    {
+        page: 22,
+        choices: ["38", "35"]
+    },
+    {
+        page: 24,
+        choices: ["6"]
+    },
+    {
+        page: 27,
+        choices: ["39", "40"]
+    },
+    {
+        page: 28,
+        choices: ["41", "42"]
+    },
+    {
+        page: 29,
+        choices: ["43", "44"]
+    },
+    {
+        page: 33,
+        choices: ["45", "45"]
+    },
+    {
+        page: 34,
+        choices: ["48", "47"]
+    },
+    {
+        page: 35,
+        choices: ["50", "53"]
+    },
+    {
+        page: 38,
+        choices: ["55", "51"]
+    },
+    {
+        page: 39,
+        choices: ["56", "57"]
+    },
+    {
+        page: 41,
+        choices: ["58", "59"]
+    },
+    {
+        page: 42,
+        choices: ["6"]
+    },
+    {
+        page: 43,
+        choices: ["60", "61"]
+    },
+    {
+        page: 44,
+        choices: ["64", "63"]
+    },
+    {
+        page: 45,
+        choices: ["65", "66"]
+    },
+    {
+        page: 46,
+        choices: ["48"]
+    },
+    {
+        page: 48,
+        choices: ["9"]
+    },
+    {
+        page: 50,
+        choices: ["67", "68"]
+    },
+    {
+        page: 51,
+        choices: ["72", "74"]
+    },
+    {
+        page: 53,
+        choices: ["69", "70"]
+    },
+    {
+        page: 55,
+        choices: ["71", "73"]
+    },
+    {
+        page: 56,
+        choices: ["75", "76"]
+    },
+    {
+        page: 57,
+        choices: ["77", "79"]
+    },
+    {
+        page: 60,
+        choices: ["80", "82"]
+    },
+    {
+        page: 61,
+        choices: ["81", "86"]
+    },
+    {
+        page: 64,
+        choices: ["63", "85", "87"]
+    },
+    {
+        page: 65,
+        choices: ["88", "89"]
+    },
+    {
+        page: 66,
+        choices: ["32"]
+    },
+    {
+        page: 67,
+        choices: ["6"]
+    },
+    {
+        page: 69,
+        choices: ["97", "98"]
+    },
+    {
+        page: 70,
+        choices: ["99", "100"]
+    },
+    {
+        page: 71,
+        choices: ["90", "91"]
+    },
+    {
+        page: 72,
+        choices: ["93", "94"]
+    },
+    {
+        page: 74,
+        choices: ["107"]
+    },
+    {
+        page: 76,
+        choices: ["108"]
+    },
+    {
+        page: 79,
+        choices: ["50"]
+    },
+    {
+        page: 81,
+        choices: ["116", "117"]
+    },
+    {
+        page: 82,
+        choices: ["112", "114"]
+    },
+    {
+        page: 88,
+        choices: ["95", "96"]
+    },
+    {
+        page: 90,
+        choices: ["101", "102"]
+    },
+    {
+        page: 91,
+        choices: ["103", "104"]
+    },
+    {
+        page: 94,
+        choices: ["105", "106"]
+    },
+    {
+        page: 96,
+        choices: ["110"]
+    },
+    {
+        page: 100,
+        choices: ["55"]
+    },
+]
